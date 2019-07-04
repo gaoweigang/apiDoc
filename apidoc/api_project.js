62956027fd68c16d9e39057a4102331a5f51a25c
gaoweigang <13817191469@163.com>
2019-07-04 15:05:27 +0800

    提交生成的apiDoc文档

---
+++ apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "apiDoc test API文档",
+  "version": "1.0.0",
+  "description": "开发环境地址：http://192.168.1.1:8080 </br> 测试环境服务地址：http://172.168.2.2:8080",
+  "apidoc": "0.3.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-07-04T06:51:10.566Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
